1632e6e6867edf33f8637d8df522015c01cb5ea7
Surodeep Sheth <sheth.surodeep@gmail.com>
2024-07-16 14:52:07 +0200

    .

---
+++ runaway_functionsv3.py
@@ -0,0 +1,337 @@
+import os
+import numpy as np
+from astropy import units as u
+from astroquery.vizier import Vizier
+from astropy.table import Table, join,vstack
+from astropy.coordinates import SkyCoord, Angle
+from astropy.time import Time
+import warnings
+from astropy.utils.metadata import MergeConflictWarning
+import time
+import logging
+from typing import List
+import yaml
+def read_yaml_file(file_path):
+    '''
+    Read the configuration file for the rest of the code. 
+    This contains the various parameters for the code to run.
+    '''
+    with open(file_path, 'r') as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+config = read_yaml_file('config2.yaml')
+
+
+class ClusterDias:
+    def __init__(self, name:str) -> None:
+        #download the catalog if not already in the cwds
+        if os.path.exists('./dias2021.tsv'):
+            dias2021 = Table.read('dias2021.tsv', format='ascii.ecsv')
+        else:
+            Vizier.ROW_LIMIT = -1
+            dias2021 = Vizier.get_catalogs(catalog="J/MNRAS/504/356/table12")[0]
+            mask_logage = dias2021['logage'] < 7.7
+            mask_plx = dias2021['Plx'] > 0.3
+            dias2021 = dias2021[mask_logage & mask_plx]
+        #select the row from Dias catalog
+        cluster_row = dias2021[dias2021['Cluster'] == name][0]
+        #cluster parameters from the row
+        self.name = name
+        self.all = cluster_row
+        self.r50 = cluster_row['r50']*u.deg
+        self.N = cluster_row['N']
+        self.skycoord = SkyCoord(ra=cluster_row['RA_ICRS']*u.deg,
+                                 dec=cluster_row['DE_ICRS']*u.deg,
+                                 distance=cluster_row['Dist']*u.pc,
+                                 pm_ra_cosdec=cluster_row['pmRA']*u.mas/u.yr,
+                                 pm_dec=cluster_row['pmDE']*u.mas/u.yr,
+                                 obstime=(Time('J2000')+1*u.Myr))
+        self.ra = self.skycoord.ra
+        self.dec = self.skycoord.dec
+        self.distance,self.e_distance = self.skycoord.distance,cluster_row['e_Dist']*u.pc
+        self.pm_ra_cosdec, self.e_pm_ra_cosdec = self.skycoord.pm_ra_cosdec,cluster_row['e_pmRA']*u.mas/u.yr
+        self.pm_dec, self.e_pm_dec = self.skycoord.pm_dec, cluster_row['e_pmDE']*u.mas/u.yr
+        self.Av, self.e_Av = cluster_row['Av']*u.mag,cluster_row['e_Av']*u.mag
+        self.logage, self.e_logage = cluster_row['logage'],cluster_row['e_logage']
+        self.FeH, self.e_FeH = cluster_row['__Fe_H_'],cluster_row['e__Fe_H_']
+        self.RV, self.e_RV = cluster_row['RV'],cluster_row['e_RV']
+        self.NRV = cluster_row['NRV']
+
+    def members(self, aslist=False):
+        members = (Table.read(f'./Clusters_Dias/{self.name}.dat', format='ascii.tab'))[2:] #first two rows removed
+        members['Source'] = members['Source'].astype(np.int64)
+        members['Pmemb'] = members['Pmemb'].astype(float)
+        members['Plx'] = members['Plx'].astype(float)
+        members['e_Plx'] = members['e_Plx'].astype(float)
+        if aslist:
+            members_list = list(members['Source'])
+            return members_list
+        else:
+            return members
+
+    def get_full_catalog() -> Table:
+        dias2021 = Vizier.get_catalogs(catalog="J/MNRAS/504/356/table12")[0]
+        dias2021.write('dias2021.tsv',format='ascii.ecsv', overwrite=True)
+        return dias2021
+
+class ClusterCG:
+    def __init__(self, name:str) -> None:
+        self.name = name
+        #download the catalog if not already in the cwds
+        if os.path.exists('./CG2020.tsv'):
+            CG2020 = Table.read('CG2020.tsv', format='ascii.ecsv')
+        else:
+            Vizier.ROW_LIMIT = -1
+            CG2020 = Vizier.get_catalogs(catalog="J/A+A/633/A99/table1")[0]
+        
+        cluster_row = CG2020[CG2020['Cluster'] == name][0]
+        self.r50 = cluster_row['r50']*u.deg
+
+    def get_full_catalog() -> Table:
+        CG2020 = Vizier.get_catalogs(catalog="J/A+A/633/A99/table1")[0]
+        CG2020.write('CG2020.tsv',format='ascii.ecsv')
+        return CG2020
+        
+class Cluster:
+    def __init__(self, name:str) -> None:
+        clusterDias = ClusterDias(name=name)
+        clusterCG = ClusterCG(name=name)
+        self.name = name
+        self.ra = clusterDias.skycoord.ra
+        self.dec = clusterDias.skycoord.dec
+        self.distance = clusterDias.skycoord.distance #to be changed DR3
+        self.r50 = clusterCG.r50
+        self.search_arcmin = search_arcmin(self.distance, self.r50)
+        # self.r50_phy = np.tan(self.r50) * self.distance #to be changed DR3
+        self.distance =10*u.pc
+        if os.path.exists(f"Clusters/{self.name}/{self.name}_members.tsv"):
+            self.mymembers = Table.read(f"Clusters/{self.name}/{self.name}_members.tsv", format="ascii.ecsv")
+        else:
+            self.mymembers = self.members()
+        # #update kinematic parameters
+        # self.distance,self.e_distance = self.mymembers['rgeo'].mean()*u.pc, self.mymembers['rgeo'].std()*u.pc
+        # self.r50_phy = np.tan(self.r50) * self.distance #changed by DR3
+        # self.changeParam(("Dist", self.distance))
+        # self.changeParam(("e_Dist", self.e_distance))
+
+        # self.pm_ra_cosdec, self.e_pm_ra_cosdec = self.mymembers['pmRA'].mean()*u.mas/u.yr, self.mymembers['pmRA'].std()*u.mas/u.yr
+        # self.changeParam(("pmRA", self.pm_ra_cosdec))
+        # self.changeParam(("e_pmRA", self.e_pm_ra_cosdec))
+
+        # self.pm_dec, self.e_pm_dec = self.mymembers['pmDE'].mean()*u.mas/u.yr, self.mymembers['pmDE'].std()*u.mas/u.yr
+        # self.changeParam(("pmDE", self.pm_dec))
+        # self.changeParam(("e_pmDE", self.e_pm_dec))
+
+
+        suro2024 = Table.read('suro2024.tsv', format='ascii.ecsv')
+        cluster_row = suro2024[suro2024['Cluster'] == name][0]
+        self.skycoord = SkyCoord(ra=cluster_row['RA_ICRS']*u.deg,
+                                dec=cluster_row['DE_ICRS']*u.deg,
+                                distance=cluster_row['Dist']*u.pc,
+                                pm_ra_cosdec=cluster_row['pmRA']*u.mas/u.yr,
+                                pm_dec=cluster_row['pmDE']*u.mas/u.yr,
+                                obstime=(Time('J2000')+1*u.Myr))
+
+        self.all = cluster_row
+        self.distance,self.e_distance = cluster_row['Dist']*u.pc,cluster_row['e_Dist']*u.pc
+        self.pm_ra_cosdec, self.e_pm_ra_cosdec = cluster_row['pmRA']*u.mas/u.yr,cluster_row['e_pmRA']*u.mas/u.yr
+        self.pm_dec, self.e_pm_dec = cluster_row['pmDE']*u.mas/u.yr, cluster_row['e_pmDE']*u.mas/u.yr
+        self.Av, self.e_Av = cluster_row['Av']*u.mag,cluster_row['e_Av']*u.mag
+        self.logage, self.e_logage = cluster_row['logage'],cluster_row['e_logage']
+        self.FeH, self.e_FeH = cluster_row['__Fe_H_'],cluster_row['e__Fe_H_']
+        self.RV, self.e_RV = cluster_row['RV'],cluster_row['e_RV']
+        self.NRV = cluster_row['NRV']
+        
+        #functions
+        # self.members = (Table.read(f'./Clusters_Dias/{self.name}.dat', format='ascii.tab'))[2:] 
+        # self.members_list = list(self.members['Source'].astype(np.int64))
+
+    def members(self, pmemb:float = config['Cluster']['pmemb'], plxquality:float = config['Cluster']['plxquality'], add_members:List = []):
+        sr = self.stars_in_region()
+        diasmembers = ClusterDias(name=self.name).members()['Source','Pmemb']
+        #add the add_members in this with Pmemb=1
+        configmembers = config.get('added_members',{}).get(self.name)
+        print(f"{configmembers} found from config file") if configmembers is not None else None
+        add_members += configmembers if configmembers is not None else []
+
+        for mem_source in add_members:
+            diasmembers.add_row([mem_source,1])
+        members = join(sr, diasmembers, keys='Source', join_type='inner') #select the ones that dias says is a member
+        print(f'{len(members)} out of {len(diasmembers)-len(add_members)} dias members found in search region')
+        mask_pmemb = members['Pmemb'] >= pmemb
+        mask_plxquality = members['Plx']/members['e_Plx'] >= plxquality
+        members = members[mask_pmemb & mask_plxquality]
+        members.sort("Gmag")
+        members.write(f"Clusters/{self.name}/{self.name}_members.tsv", format="ascii.ecsv", overwrite=True)
+        #update kinematic parameters
+        self.changeParam(("N", len(members)))
+        self.changeParam(("Plx", members['Plx'].mean()))
+        self.changeParam(("e_Plx", members['Plx'].std()))
+        self.changeParam(("Dist", members['rgeo'].mean()))
+        self.changeParam(("e_Dist", members['rgeo'].std()))
+        self.changeParam(("pmRA", members['pmRA'].mean()))
+        self.changeParam(("e_pmRA", members['pmRA'].std()))
+        self.changeParam(("pmDE", members['pmDE'].mean()))
+        self.changeParam(("e_pmDE", members['pmRA'].std()))
+        self.changeParam(("RV", members['RV'].mean()))
+        self.changeParam(("e_RV", members['RV'].std()))
+        self.changeParam(("NRV", (~members['RV'].mask).sum()))
+        return members
+
+    def stars_in_region(self):
+        stars_in_region_path =  f'Clusters/{self.name}/{self.name}_stars_in_region.tsv'
+        
+        if os.path.exists(stars_in_region_path):
+            stars_in_region = Table.read(stars_in_region_path, format='ascii.ecsv')
+        else:
+            stars_in_region = self.get_stars_in_region()
+            stars_in_region.write(stars_in_region_path, format='ascii.ecsv')
+        return stars_in_region
+
+    def get_stars_in_region(self) -> Table:
+        c = ClusterDias(self.name).skycoord
+        t1 = searchDR3(c,self.search_arcmin)
+        t2 = searchDR3_dist(c,self.search_arcmin)
+        t3 = merge_gaia_tables(t1,t2)
+        t3.sort('Gmag')
+        return t3
+    
+    def changeParam(self,change: tuple):
+        param,new_value = change
+        cluster_list = Table.read('dias2021.tsv', format='ascii.ecsv')
+        cluster_list_mod = Table.read('suro2024.tsv', format='ascii.ecsv')
+        _old_value  = cluster_list[cluster_list['Cluster'] == self.name][0][param]
+        cluster_list_mod[param][cluster_list_mod['Cluster'] == self.name] = new_value
+        cluster_list_mod.write('suro2024.tsv', format='ascii.ecsv', overwrite=True)
+        warnings.simplefilter('ignore', FutureWarning)
+        print(f'Changed {param:10} {_old_value:.2f} --> {new_value:.2f}')
+    
+
+    def restoreParam(self, param: str):
+        # Read the original and modified tables
+        cluster_list_original = Table.read('dias2021.tsv', format='ascii.ecsv')
+        cluster_list_modified = Table.read('suro2024.tsv', format='ascii.ecsv')
+        # Get the name of the cluster
+        cluster_name = self.name
+        if param == 'all':
+            # Iterate over all columns in the original table
+            for column in cluster_list_original.colnames:
+                # Get the original value for the current column
+                original_value = cluster_list_original[cluster_list_original['Cluster'] == cluster_name][0][column]
+                # Get the current value for the current column in the modified table (for logging)
+                current_value = cluster_list_modified[cluster_list_modified['Cluster'] == cluster_name][0][column]
+                # Restore the original value in the modified table
+                cluster_list_modified[column][cluster_list_modified['Cluster'] == cluster_name] = original_value
+                if current_value != original_value:
+                    print(f'Restored {column}: current {current_value} --> {original_value}')
+        else:
+            # Handle the case for a single parameter as before
+            original_value = cluster_list_original[cluster_list_original['Cluster'] == cluster_name][0][param]
+            current_value = cluster_list_modified[cluster_list_modified['Cluster'] == cluster_name][0][param]
+            
+            cluster_list_modified[param][cluster_list_modified['Cluster'] == cluster_name] = original_value
+            print(f'Restored {param}: current {current_value} --> {original_value}')
+        # Save the modified table
+        cluster_list_modified.write('suro2024.tsv', format='ascii.ecsv', overwrite=True)
+
+
+
+
+
+
+
+# Configure logging
+def search_arcmin(distance, radius:Angle, extra=config['Cluster']['search_extent']):
+    theta = radius
+    D = distance
+    r = np.tan(theta) * D #physical radius
+    search_arcmin = np.arctan((r + extra * u.pc) / D)
+    search_arcmin = search_arcmin.to(u.arcminute)
+    return search_arcmin.round(3)
+
+
+def searchDR3(skycoordinate: SkyCoord, radius: Angle) -> Table:
+    start_time = time.time()
+    print(f"Searching in Gaia DR3 I/355/gaiadr3 {radius} around {skycoordinate.ra, skycoordinate.dec, skycoordinate.distance}")
+
+    filters = {'Gmag': '<17', 'Plx': '>0.3'}
+    stars_fromDR3 = Vizier(columns=["*", "+_r"], row_limit=-1).query_region(
+        skycoordinate, 
+        radius=radius, 
+        catalog="I/355/gaiadr3",
+        column_filters=filters
+    )[0]
+    
+    stars_fromDR3['SkyCoord1'] = SkyCoord(
+        ra=stars_fromDR3['RA_ICRS'],
+        dec=stars_fromDR3['DE_ICRS'],
+        distance=(stars_fromDR3['Plx']).to(u.pc, equivalencies=u.parallax()),
+        pm_ra_cosdec=stars_fromDR3['pmRA'],
+        pm_dec=stars_fromDR3['pmDE'],
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+
+    end_time = time.time()
+    print(f"found {len(stars_fromDR3):,} sources in {end_time - start_time:.2f} seconds")
+    return stars_fromDR3
+
+def searchDR3_dist(skycoordinate: SkyCoord, radius: Angle) -> Table:
+    start_time = time.time()
+    print(f"Searching in Gaia DR3 distances I/352/gedr3dis {radius} around {skycoordinate.ra, skycoordinate.dec, skycoordinate.distance}")
+
+
+    stars_fromDR3_dist = Vizier(columns=["*", "+_r"], row_limit=-1).query_region(
+        skycoordinate, 
+        radius=radius, 
+        catalog="I/352/gedr3dis"
+    )[0]
+    
+    stars_fromDR3_dist['SkyCoord2'] = SkyCoord(
+        ra=stars_fromDR3_dist['RA_ICRS'],
+        dec=stars_fromDR3_dist['DE_ICRS'],
+        distance=(stars_fromDR3_dist['rgeo']),
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+
+    end_time = time.time()
+    print(f"found {len(stars_fromDR3_dist):,} sources in {end_time - start_time:.2f} seconds")
+    filtered_catalog = stars_fromDR3_dist[skycoordinate.separation_3d(stars_fromDR3_dist['SkyCoord2']) < skycoordinate.distance*config['Cluster']['distance_tolerance']]
+    return filtered_catalog
+
+def merge_gaia_tables(stars_fromDR3: Table, stars_fromDR3_dist: Table) -> Table:
+    start_time = time.time()
+    print("Starting merge of DR3 and distance catalog data")
+
+    warnings.simplefilter('ignore', MergeConflictWarning)
+    merged = join(stars_fromDR3, stars_fromDR3_dist, keys='Source', join_type='inner')
+
+    # Order the columns
+    merged = merged['RA_ICRS_1', 'DE_ICRS_1', 'e_RA_ICRS', 'e_DE_ICRS', '_r_1',
+                                       'HIP', 'TYC2', 'Source', 'rgeo', 'Plx', 'e_Plx',
+                                       'pmRA', 'pmDE', 'e_pmRA', 'e_pmDE',
+                                       'RUWE', 'Teff', 'logg', 'Gmag', 'BP-RP', 'BPmag', 'RPmag', 'RV', 'e_RV',
+                                       'b_rgeo', 'B_rgeo', 'FG', 'e_FG', 'FBP', 'e_FBP', 'FRP', 'e_FRP', 'RAVE5', 'RAVE6']
+
+    # Adding row for uncertainties in Gmag and BPmag and RPmag
+    # values for Gaia G, G_BP, G_RP zero point uncertainties
+    sigmaG_0 = 0.0027553202
+    sigmaGBP_0 = 0.0027901700
+    sigmaGRP_0 = 0.0037793818
+
+    merged['e_Gmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FG'] / merged['FG'])**2 + sigmaG_0**2)
+    merged['e_BPmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FBP'] / merged['FBP'])**2 + sigmaGBP_0**2)
+    merged['e_RPmag'] = np.sqrt((-2.5 / np.log(10) * merged['e_FRP'] / merged['FRP'])**2 + sigmaGRP_0**2)
+    merged['e_BP-RP'] = merged['e_BPmag'] + merged['e_RPmag']
+
+    merged['SkyCoord'] = SkyCoord(
+        ra=merged['RA_ICRS_1'],
+        dec=merged['DE_ICRS_1'],
+        distance=(merged['rgeo']),
+        pm_ra_cosdec=merged['pmRA'],
+        pm_dec=merged['pmDE'],
+        obstime=(Time('J2000') + 1 * u.Myr)
+    )
+
+    end_time = time.time()
+    print(f"{len(merged):,} sources found by merging in {end_time - start_time:.2f} seconds")
+    return merged
